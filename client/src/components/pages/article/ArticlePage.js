eff46e18d1ca6b818d9eed6263f06f10524d9ebf
L-AB <lesinternets@tutanota.com>
2020-09-13 23:49:03 +0200

    structure article page

--- client/src/components/pages/article/ArticlePage.js
+++ client/src/components/pages/article/ArticlePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react'
 import axios from 'axios'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { device } from '../../cors/ResponsiveSettings'
 import { BlogContext } from '../../../context/BlogContext'
 import ArticleCard from '../home/ArticleCard'
@@ -47,9 +47,18 @@ const ArticlePage = (props) => {
       {article && (
         <Wrapper ref={element}>
 
-          <ImgWrapper src={article.picture} />
+          <ImgWrapper src={article.picture} main />
           <h3>{article.title}</h3>
-          <p>{article.text}</p>
+          <p>{article.text1}</p>
+          <ImgWrapper src={article.picture2} />
+          <h4>{article.title2}</h4>
+          <p>{article.text2}</p>
+          <ImgWrapper src={article.picture3} />
+          <h4>{article.title3}</h4>
+          <p>{article.text3}</p>
+          <ImgWrapper src={article.picture4} />
+          <h4>{article.title4}</h4>
+          <p>{article.text4}</p>
 
         </Wrapper>
       )
@@ -62,22 +71,29 @@ export default ArticlePage
 
 const Wrapper = styled.div`
   padding: 4rem 2rem;
-  h3{
-    font-size:1.5rem;
-  }
   p{
-    font-size: 1.25rem;
+    padding: 1rem 0;
   }
   @media ${device.laptop} {
     padding:10vw;
   }
 `
 const ImgWrapper = styled.img`
+    --ratio: 135 / 240;
   width:100%;
+  height: calc(100% * var(--ratio));
+  object-position: top;
+  object-fit: cover;
+  padding: 1rem 0;
   @media ${device.laptop} {
     width:28vw;
-    height:18vw;
-    object-position: top;
-    object-fit: cover;
+    height: calc(28vw * var(--ratio));
+    ${props =>
+    props.main &&
+    css`
+      width:48vw;
+      height: calc(48vw * var(--ratio));
+    `}
+
   }
 `
